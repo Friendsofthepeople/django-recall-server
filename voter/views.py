a608e613309a6c6dba29323d6ce65b98634cfeff
Collins <134794051+comony@users.noreply.github.com>
2024-06-20 08:06:03 +0000

    add voter registration

---
+++ voter/views.py
@@ -0,0 +1,13 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Voter
+from .serializers import VoterSerializer
+
+class VoterRegisterView(APIView):
+    def post(self, request):
+        serializer = VoterSerializer(data=request.data)
+        if serializer.is_valid():
+            voter = serializer.save()
+            return Response(VoterSerializer(voter).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
