47c1d99d253fa7ed08c645de65e9962b2aa013a4
Collins' Ghost <134794051+comony@users.noreply.github.com>
2024-06-22 14:22:17 +0000

    update mps model

--- mps/models.py
+++ mps/models.py
@@ -2,13 +2,15 @@ from django.db import models
 import uuid
 
 class MemberOfParliament(models.Model):
-    first_name = models.CharField(max_length=100)
-    middle_name = models.CharField(max_length=100, blank=True, null=True)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    image = models.URLField(max_length=200, default='https://img.freepik.com/free-vector/illustration-businessman_53876-5856.jpg?w=740&t=st=1719052890~exp=1719053490~hmac=4e6f9ee8ae73ee004e482dc13a877e58d9dc91abcc31b2f6cfe9d878199eaba6')
+    county = models.CharField(max_length=100)
     constituency = models.CharField(max_length=100)
     party = models.CharField(max_length=100)
+
     tokenized_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name} {self.surname} - {self.constituency}"
--- mps/serializers.py
+++ mps/serializers.py
@@ -4,7 +4,7 @@ from .models import MemberOfParliament
 class MemberOfParliamentSerializer(serializers.ModelSerializer):
     class Meta:
         model = MemberOfParliament
-        fields = ['id', 'first_name', 'middle_name', 'last_name', 'constituency', 'party', 'tokenized_id', 'created_at']
+        fields = ['id', 'image', 'name', 'county', 'constituency', 'party', 'tokenized_id', 'created_at']
         read_only_fields = ['tokenized_id', 'created_at']
 
     def create(self, validated_data):
