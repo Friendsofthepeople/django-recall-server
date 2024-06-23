98c8c9deb49739948a14d47e32ac2ea5ada3e62c
carlton <carltonagesa@gmail.com>
2024-06-23 11:23:27 +0300

    Add Swagger & OpenAi Documentation

--- recallServer/urls.py
+++ recallServer/urls.py
@@ -6,6 +6,23 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Member of Parliament Recall System",
+        default_version='v1.0',
+        description="A Django server for managing the verification of constituents, publishing of representative information, and signing to recall them.",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT license"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -15,4 +32,6 @@ urlpatterns = [
     path('api/voter/', include('voter.urls')),
     path('api/mps/', include('mps.urls')),
     path('api/recall/', include('recall.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
