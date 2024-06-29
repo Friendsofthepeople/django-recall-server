65932b1c0f773ec57db06e11719865ee83a9063a
c-o-m-o-n <comon928@gmail.com>
2024-06-29 13:29:52 +0000

    update settings

--- recall_server/config/settings.py
+++ recall_server/config/settings.py
@@ -4,7 +4,7 @@ from decouple import config  # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'config("SECRET_KEY")'
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
 CSRF_TRUSTED_ORIGINS = ["https://localhost:8000"]
