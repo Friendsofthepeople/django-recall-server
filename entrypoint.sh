a2bd94c2dbc2dd9d2071b807d83dd4640ce18ab3
Collins O. Odhiambo <comon928@gmail.com>
2024-07-06 10:19:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-set -e
-
+# Apply database migrations
+echo "Applying database migrations..."
 python manage.py migrate
 
 echo "Starting Gunicorn."
-exec gunicorn phantom_wrap.config.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --access-logfile - --error-logfile - --log-level info \
-    "$@"
+# Start the server
+echo "Starting server..."
+exec "$@"
