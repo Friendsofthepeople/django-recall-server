1ec719a5154f9ae3633cb831d91b21becb0fc007
17Keith <kayomega7@gmail.com>
2024-07-13 17:04:15 +0300

    Add admin func to voters

--- recall_server/voter/admin.py
+++ recall_server/voter/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Voter
 
+class VoterAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "is_active", "created_at")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("is_active", "created_at")
+
+fielsets = (
+    (None, {
+        "fields": ("first_name", "last_name", "email")
+    }),
+    ("permissions", {"fields": ("is_active",)}),
+    ("Important dates", {"fields":("created_at",)}),
+)
+
 # Register your models here.
-admin.site.register(Voter)
+admin.site.register(Voter, VoterAdmin)
