f0167a921cf7e97912f37cf605dfd0dbd1c0829b
Collins O. Odhiambo <comon928@gmail.com>
2024-08-12 14:10:22 +0300

    Update settings.py:  rm render config from settings

--- recall_server/config/settings.py
+++ recall_server/config/settings.py
@@ -145,14 +145,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = Path(BASE_DIR, "staticfiles")
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
