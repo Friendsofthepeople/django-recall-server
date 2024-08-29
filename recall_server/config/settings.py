2fecd2a57eea7977ad9624286d6a3beff623ebca
Collins O. Odhiambo <comon928@gmail.com>
2024-08-29 19:19:20 +0300

    Update settings.py

--- recall_server/config/settings.py
+++ recall_server/config/settings.py
@@ -7,6 +7,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import os
 
 from decouple import config
 
@@ -144,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
